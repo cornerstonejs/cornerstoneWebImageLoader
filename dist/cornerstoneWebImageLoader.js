451df54bae2f27f5f3ef2f344370c3eabc9c311f
Chris Hafey <chafey@gmail.com>
2014-09-21 19:59:30 -0500

    bumped cornerstone lib version

--- dist/cornerstoneWebImageLoader.js
+++ dist/cornerstoneWebImageLoader.js
@@ -1,4 +1,4 @@
-/*! cornerstoneWebImageLoader - v0.3.0 - 2014-07-30 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstoneWebImageLoader */
+/*! cornerstoneWebImageLoader - v0.3.1 - 2014-09-21 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstoneWebImageLoader */
 //
 // This is a cornerstone image loader for web images such as PNG and JPEG
 //
